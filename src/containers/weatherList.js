a74d99023e28814071c0fc38ad9ce5dd987b5122
Sonja Olson <sonjaolson@Sonjas-MacBook-Air.local>
2018-04-17 15:14:40 -0700

    adding sparklines to table

---
+++ src/containers/weatherList.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
+class WeatherList extends Component{
+
+renderWeather(cityData){
+  const name = cityData.city.name;
+  const temps = cityData.list.map(weather => weather.main.temp);
+  const pressure = cityData.list.map(weather => weather.main.pressure);
+  const humidity = cityData.list.map(weather => weather.main.humidity);
+  return(
+    <tr key={name}>
+      <td>{name}</td>
+      <td><Sparklines height={120} width={180} data={temps}>
+        <SparklinesLine color="blue"/>
+        </Sparklines>
+      </td>
+      <td><Sparklines height={120} width={180} data={pressure}>
+        <SparklinesLine color="green"/>
+        </Sparklines>
+      </td>
+      <td><Sparklines height={120} width={180} data={humidity}>
+        <SparklinesLine color="red"/>
+        </Sparklines>
+      </td>
+      </tr>
+  )
+}
+
+  render(){
+    return(
+      <table className='table table-hover'>
+        <thead>
+          <tr>
+            <th> City </th>
+            <th> Temperature </th>
+            <th> Pressure </th>
+            <th> Humidity </th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.weather.map(this.renderWeather)}
+          </tbody>
+        </table>
+    );
+  }
+}
+
+function mapStateToProps({weather}){
+  return {weather};
+}
+
+export default connect(mapStateToProps)(WeatherList);
